bcb0526e97029b196a45472aaf3620a53933814a
PrajwalLuitel <prajwalluitel@gmail.com>
2024-02-21 11:33:58 -0500

    21/02/2024

---
+++ longest_substring_without_repeating/solution.py
@@ -0,0 +1,55 @@
+"""
+Given a string s, find the length of the longest 
+substring
+ without repeating characters.
+
+ 
+
+Example 1:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+Example 2:
+
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Example 3:
+
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ 
+
+Constraints:
+
+0 <= s.length <= 5 * 104
+s consists of English letters, digits, symbols and spaces.
+"""
+
+
+
+def lengthOfLongestSubstring(s: str) -> int:
+    elements:list[str] = []
+    left:int = 0
+
+    max_length:int = 0
+
+    for right in range(len(s)):
+        if s[right] not in elements:
+            elements.append(s[right])
+            max_length = max(max_length, right-left+1)
+        
+        else:
+            while s[right] in elements:
+                elements.remove(s[left])
+                left += 1
+            elements.append(s[right])
+        print(f"iteration: {right}: set: {elements}")
+
+    return max_length
+
+
+print(lengthOfLongestSubstring("abdefaezqwtyrep"))
